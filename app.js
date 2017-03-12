b5ae5aacc9c31dc032f8d6438375fc6f27bc3da9
Łukasz Krawczyk <ukasz Krawczyk>
2017-03-12 14:48:12 +0100

    update - add variable

--- app.js
+++ app.js
@@ -8,7 +8,9 @@ $('button.new-game').on('click', function(){
     location.reload();
 })
 
-$('.game-conteiner div').on('click', function(){
+var $field = $('.game-conteiner div');
+
+$field.on('click', function(){
     
     if($(this).hasClass('')){
         if (count % 2 === 1) {
@@ -20,131 +22,131 @@ $('.game-conteiner div').on('click', function(){
         }
     }
 
-    if ($('.game-conteiner div').eq(0).hasClass('x') &&  $('.game-conteiner div').eq(1).hasClass('x') && $('.game-conteiner div').eq(2).hasClass('x')) {
-        $('.game-conteiner div').eq(0).addClass('win');
-        $('.game-conteiner div').eq(1).addClass('win');
-        $('.game-conteiner div').eq(2).addClass('win');
-        $('.game-conteiner div').off();
+    if ($field.eq(0).hasClass('x') &&  $field.eq(1).hasClass('x') && $field.eq(2).hasClass('x')) {
+        $field.eq(0).addClass('win');
+        $field.eq(1).addClass('win');
+        $field.eq(2).addClass('win');
+        $field.off();
         winGame();
     }
 
-    if ($('.game-conteiner div').eq(0).hasClass('o') &&  $('.game-conteiner div').eq(1).hasClass('o') && $('.game-conteiner div').eq(2).hasClass('o')) {
-        $('.game-conteiner div').eq(0).addClass('win');
-        $('.game-conteiner div').eq(1).addClass('win');
-        $('.game-conteiner div').eq(2).addClass('win');
-        $('.game-conteiner div').off();
+    if ($field.eq(0).hasClass('o') &&  $field.eq(1).hasClass('o') && $field.eq(2).hasClass('o')) {
+        $field.eq(0).addClass('win');
+        $field.eq(1).addClass('win');
+        $field.eq(2).addClass('win');
+        $field.off();
         winGame();
     }
 
-    if ($('.game-conteiner div').eq(3).hasClass('x') &&  $('.game-conteiner div').eq(4).hasClass('x') && $('.game-conteiner div').eq(5).hasClass('x')) {
-        $('.game-conteiner div').eq(3).addClass('win');
-        $('.game-conteiner div').eq(4).addClass('win');
-        $('.game-conteiner div').eq(5).addClass('win');
-        $('.game-conteiner div').off();
+    if ($field.eq(3).hasClass('x') &&  $field.eq(4).hasClass('x') && $field.eq(5).hasClass('x')) {
+        $field.eq(3).addClass('win');
+        $field.eq(4).addClass('win');
+        $field.eq(5).addClass('win');
+        $field.off();
         winGame();
     }
 
-    if ($('.game-conteiner div').eq(3).hasClass('o') &&  $('.game-conteiner div').eq(4).hasClass('o') && $('.game-conteiner div').eq(5).hasClass('o')) {
-        $('.game-conteiner div').eq(3).addClass('win');
-        $('.game-conteiner div').eq(4).addClass('win');
-        $('.game-conteiner div').eq(5).addClass('win');
-        $('.game-conteiner div').off();
+    if ($field.eq(3).hasClass('o') &&  $field.eq(4).hasClass('o') && $field.eq(5).hasClass('o')) {
+        $field.eq(3).addClass('win');
+        $field.eq(4).addClass('win');
+        $field.eq(5).addClass('win');
+        $field.off();
         winGame();
     }
 
-    if ($('.game-conteiner div').eq(6).hasClass('x') &&  $('.game-conteiner div').eq(7).hasClass('x') && $('.game-conteiner div').eq(8).hasClass('x')) {
-        $('.game-conteiner div').eq(6).addClass('win');
-        $('.game-conteiner div').eq(7).addClass('win');
-        $('.game-conteiner div').eq(8).addClass('win');
-        $('.game-conteiner div').off();
+    if ($field.eq(6).hasClass('x') &&  $field.eq(7).hasClass('x') && $field.eq(8).hasClass('x')) {
+        $field.eq(6).addClass('win');
+        $field.eq(7).addClass('win');
+        $field.eq(8).addClass('win');
+        $field.off();
         winGame();
     }
 
-    if ($('.game-conteiner div').eq(6).hasClass('o') &&  $('.game-conteiner div').eq(7).hasClass('o') && $('.game-conteiner div').eq(8).hasClass('o')) {
-        $('.game-conteiner div').eq(6).addClass('win');
-        $('.game-conteiner div').eq(7).addClass('win');
-        $('.game-conteiner div').eq(8).addClass('win');
-        $('.game-conteiner div').off();
+    if ($field.eq(6).hasClass('o') &&  $field.eq(7).hasClass('o') && $field.eq(8).hasClass('o')) {
+        $field.eq(6).addClass('win');
+        $field.eq(7).addClass('win');
+        $field.eq(8).addClass('win');
+        $field.off();
         winGame();
     }
 
-    if ($('.game-conteiner div').eq(0).hasClass('x') &&  $('.game-conteiner div').eq(3).hasClass('x') && $('.game-conteiner div').eq(6).hasClass('x')) {
-        $('.game-conteiner div').eq(0).addClass('win');
-        $('.game-conteiner div').eq(3).addClass('win');
-        $('.game-conteiner div').eq(6).addClass('win');
-        $('.game-conteiner div').off();
+    if ($field.eq(0).hasClass('x') &&  $field.eq(3).hasClass('x') && $field.eq(6).hasClass('x')) {
+        $field.eq(0).addClass('win');
+        $field.eq(3).addClass('win');
+        $field.eq(6).addClass('win');
+        $field.off();
         winGame();
     }
 
-    if ($('.game-conteiner div').eq(0).hasClass('o') &&  $('.game-conteiner div').eq(3).hasClass('o') && $('.game-conteiner div').eq(6).hasClass('o')) {
-        $('.game-conteiner div').eq(0).addClass('win');
-        $('.game-conteiner div').eq(3).addClass('win');
-        $('.game-conteiner div').eq(6).addClass('win');
-        $('.game-conteiner div').off();
+    if ($field.eq(0).hasClass('o') &&  $field.eq(3).hasClass('o') && $field.eq(6).hasClass('o')) {
+        $field.eq(0).addClass('win');
+        $field.eq(3).addClass('win');
+        $field.eq(6).addClass('win');
+        $field.off();
         winGame();
     }
 
-    if ($('.game-conteiner div').eq(1).hasClass('x') &&  $('.game-conteiner div').eq(4).hasClass('x') && $('.game-conteiner div').eq(7).hasClass('x')) {
-        $('.game-conteiner div').eq(1).addClass('win');
-        $('.game-conteiner div').eq(4).addClass('win');
-        $('.game-conteiner div').eq(7).addClass('win');
-        $('.game-conteiner div').off();
+    if ($field.eq(1).hasClass('x') &&  $field.eq(4).hasClass('x') && $field.eq(7).hasClass('x')) {
+        $field.eq(1).addClass('win');
+        $field.eq(4).addClass('win');
+        $field.eq(7).addClass('win');
+        $field.off();
         winGame();
     }
 
-    if ($('.game-conteiner div').eq(1).hasClass('o') &&  $('.game-conteiner div').eq(4).hasClass('o') && $('.game-conteiner div').eq(7).hasClass('o')) {
-        $('.game-conteiner div').eq(1).addClass('win');
-        $('.game-conteiner div').eq(4).addClass('win');
-        $('.game-conteiner div').eq(7).addClass('win');
-        $('.game-conteiner div').off();
+    if ($field.eq(1).hasClass('o') &&  $field.eq(4).hasClass('o') && $field.eq(7).hasClass('o')) {
+        $field.eq(1).addClass('win');
+        $field.eq(4).addClass('win');
+        $field.eq(7).addClass('win');
+        $field.off();
         winGame();
     }
 
-    if ($('.game-conteiner div').eq(2).hasClass('x') &&  $('.game-conteiner div').eq(5).hasClass('x') && $('.game-conteiner div').eq(8).hasClass('x')) {
-        $('.game-conteiner div').eq(2).addClass('win');
-        $('.game-conteiner div').eq(5).addClass('win');
-        $('.game-conteiner div').eq(8).addClass('win');
-        $('.game-conteiner div').off();
+    if ($field.eq(2).hasClass('x') &&  $field.eq(5).hasClass('x') && $field.eq(8).hasClass('x')) {
+        $field.eq(2).addClass('win');
+        $field.eq(5).addClass('win');
+        $field.eq(8).addClass('win');
+        $field.off();
         winGame();
     }
 
-    if ($('.game-conteiner div').eq(2).hasClass('o') &&  $('.game-conteiner div').eq(5).hasClass('o') && $('.game-conteiner div').eq(8).hasClass('o')) {
-        $('.game-conteiner div').eq(2).addClass('win');
-        $('.game-conteiner div').eq(5).addClass('win');
-        $('.game-conteiner div').eq(8).addClass('win');
-        $('.game-conteiner div').off();
+    if ($field.eq(2).hasClass('o') &&  $field.eq(5).hasClass('o') && $field.eq(8).hasClass('o')) {
+        $field.eq(2).addClass('win');
+        $field.eq(5).addClass('win');
+        $field.eq(8).addClass('win');
+        $field.off();
         winGame();
     }
 
-    if ($('.game-conteiner div').eq(0).hasClass('x') &&  $('.game-conteiner div').eq(4).hasClass('x') && $('.game-conteiner div').eq(8).hasClass('x')) {
-        $('.game-conteiner div').eq(0).addClass('win');
-        $('.game-conteiner div').eq(4).addClass('win');
-        $('.game-conteiner div').eq(8).addClass('win');
-        $('.game-conteiner div').off();
+    if ($field.eq(0).hasClass('x') &&  $field.eq(4).hasClass('x') && $field.eq(8).hasClass('x')) {
+        $field.eq(0).addClass('win');
+        $field.eq(4).addClass('win');
+        $field.eq(8).addClass('win');
+        $field.off();
         winGame();
     }
     
-    if ($('.game-conteiner div').eq(0).hasClass('o') &&  $('.game-conteiner div').eq(4).hasClass('o') && $('.game-conteiner div').eq(8).hasClass('o')) {
-        $('.game-conteiner div').eq(0).addClass('win');
-        $('.game-conteiner div').eq(4).addClass('win');
-        $('.game-conteiner div').eq(8).addClass('win');
-        $('.game-conteiner div').off();
+    if ($field.eq(0).hasClass('o') &&  $field.eq(4).hasClass('o') && $field.eq(8).hasClass('o')) {
+        $field.eq(0).addClass('win');
+        $field.eq(4).addClass('win');
+        $field.eq(8).addClass('win');
+        $field.off();
         winGame();
     }
 
-    if ($('.game-conteiner div').eq(2).hasClass('x') &&  $('.game-conteiner div').eq(4).hasClass('x') && $('.game-conteiner div').eq(6).hasClass('x')) {
-        $('.game-conteiner div').eq(2).addClass('win');
-        $('.game-conteiner div').eq(4).addClass('win');
-        $('.game-conteiner div').eq(6).addClass('win');
-        $('.game-conteiner div').off();
+    if ($field.eq(2).hasClass('x') &&  $field.eq(4).hasClass('x') && $field.eq(6).hasClass('x')) {
+        $field.eq(2).addClass('win');
+        $field.eq(4).addClass('win');
+        $field.eq(6).addClass('win');
+        $field.off();
         winGame();
     }
 
-    if ($('.game-conteiner div').eq(2).hasClass('o') &&  $('.game-conteiner div').eq(4).hasClass('o') && $('.game-conteiner div').eq(6).hasClass('o')) {
-        $('.game-conteiner div').eq(2).addClass('win');
-        $('.game-conteiner div').eq(4).addClass('win');
-        $('.game-conteiner div').eq(6).addClass('win');
-        $('.game-conteiner div').off();
+    if ($field.eq(2).hasClass('o') &&  $field.eq(4).hasClass('o') && $field.eq(6).hasClass('o')) {
+        $field.eq(2).addClass('win');
+        $field.eq(4).addClass('win');
+        $field.eq(6).addClass('win');
+        $field.off();
         winGame();
     }
 
